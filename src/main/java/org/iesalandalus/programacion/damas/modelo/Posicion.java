be2fcb7f13e7793137222ddb14f2c2affa81d4d1
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-03 08:07:10 +0100

    Apartado 3.3 y 3.4: faltaba el try y el catch. Creación del contructor copia

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -7,12 +7,33 @@ public class Posicion {
     private static final int FILA_MIN = 1;
     private static final int FILA_MAX = 8;
 
-
+    //Constructor
     public Posicion(int fila, char columna){
-        setFila(fila);
-        setColumna(columna);
+
+        //Comprobar fila
+        try{
+            setFila(fila);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Fila incorrecta: " + e.getMessage());
+        }
+
+        // Comprobar Columna
+        try{
+            setColumna(columna);
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("Columna incorrecta: " + e.getMessage());
+        }
     }
 
+    //Constructor Copia
+    public Posicion(Posicion posicon){
+        this.fila = posicon.fila;
+        this.columna = posicon.columna;
+    }
+
+    // Metodos GET
     public int getFila(){
         return fila;
     }
@@ -21,18 +42,19 @@ public class Posicion {
         return columna;
     }
 
+    // Metodos SET
     private void setFila(int fila){
         if(fila < FILA_MIN){
-            throw new IllegalArgumentException("ERROR: La fila no puede ser menor a 1");
+            throw new IllegalArgumentException("La fila no puede ser menor a 1");
         } else if (fila > FILA_MAX) {
-            throw new IllegalArgumentException("ERROR: La fila no puede ser mayor a 8");
+            throw new IllegalArgumentException("La fila no puede ser mayor a 8");
         }
         this.fila = fila;
     }
 
     private void setColumna(char columna){
        if (columna != 'a' && columna != 'b' && columna != 'c' && columna != 'd' && columna != 'e' && columna != 'f' && columna != 'g' && columna != 'h'){
-           throw new IllegalArgumentException("ERROR: La columna solo puede ser desde a hasta h");
+           throw new IllegalArgumentException("La columna solo puede ser desde a hasta h");
        }
        this.columna = columna;
     }
