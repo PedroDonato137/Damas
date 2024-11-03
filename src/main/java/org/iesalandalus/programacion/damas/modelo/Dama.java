1fc5ee72d82e819c260b3f8311d6360b2321232e
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-03 10:53:19 +0100

    Apartado 4.2: Creación del GET y SET

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -5,4 +5,22 @@ public class Dama {
     private Color color;
     private Posicion posicion;
     private boolean esDamaEspecial;
+
+    // Métodos GET
+    public Color getColor() {
+        return color;
+    }
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    //Métodos SET
+    private void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
 }
