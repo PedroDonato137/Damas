e425791e0f8410eb321183d71504e9ad25f3b473
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-03 07:29:32 +0100

    Apartado 3.2: Creación de get y set con sus validaciones

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -3,4 +3,31 @@ package org.iesalandalus.programacion.damas.modelo;
 public class Posicion {
     private int fila;
     private char columna;
+
+    private static final int FILA_MIN = 1;
+    private static final int FILA_MAX = 8;
+
+    public int getFila(){
+        return fila;
+    }
+
+    public int getColumna(){
+        return columna;
+    }
+
+    private void setFila(int fila){
+        if(fila < FILA_MIN){
+            throw new IllegalArgumentException("ERROR: La fila no puede ser menor a 1");
+        } else if (fila > FILA_MAX) {
+            throw new IllegalArgumentException("ERROR: La fila no puede ser mayor a 8");
+        }
+        this.fila = fila;
+    }
+
+    private void setColumna(char columna){
+       if (columna != 'a' && columna != 'b' && columna != 'c' && columna != 'd' && columna != 'e' && columna != 'f' && columna != 'g' && columna != 'h'){
+           throw new IllegalArgumentException("ERROR: La columna solo puede ser desde a hasta h");
+       }
+       this.columna = columna;
+    }
 }
