470d0f73673929831d47e12c79bfa9c4afe86086
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-05 00:07:47 +0100

    Apartado 5.1.1 y 5.4 Corrección de errores para los test

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.damas.modelo;
 
+import javax.naming.OperationNotSupportedException;
 import java.util.Objects;
 
 public class Dama {
@@ -12,17 +13,17 @@ public class Dama {
     private static final Boolean DAMAESPECIAL_INICIAL = false;
 
     //Constructor por defecto
-    public Dama(){
-        this.color = COLOR_INICIAL;
-        this.posicion = crearPosicionInicial();
+    public Dama() {
+        setColor(COLOR_INICIAL);
+        setPosicion(crearPosicionInicial());
         this.esDamaEspecial = DAMAESPECIAL_INICIAL;
     }
 
     //Contructor para la clase
-    public Dama(Color color){
+    public Dama(Color color) {
         Objects.requireNonNull(color, "ERROR: El color no puede ser nulo.");
-        this.color = color;
-        this.posicion = crearPosicionInicial();
+        setColor(color);
+        setPosicion(crearPosicionInicial());
         this.esDamaEspecial = DAMAESPECIAL_INICIAL;
     }
 
@@ -30,6 +31,7 @@ public class Dama {
     public Color getColor() {
         return color;
     }
+
     public Posicion getPosicion() {
         return posicion;
     }
@@ -43,74 +45,160 @@ public class Dama {
         this.posicion = posicion;
     }
 
-    public static char columnaAleatoria(int filaFicha){
+    // Métodos crearPosicionInicial
+    private Posicion crearPosicionInicial() { // Apartado 4.1
 
+        int posicionFila = 0;
         int columnaAleatoria = (int) Math.floor(Math.random() * 4) + 1; // Genera un número aleatorio desde 1 a 4
         char posicionColumna = ' ';
 
-        if (filaFicha%2 == 0){
-            switch (columnaAleatoria){
-                case 1:
-                    posicionColumna = 'b';
-                case 2:
-                    posicionColumna = 'd';
-                case 3:
-                    posicionColumna = 'f';
-                case 4:
-                    posicionColumna = 'h';
-            }
-        }else{
-            switch (columnaAleatoria){
-                case 1:
-                    posicionColumna = 'a';
-                case 2:
-                    posicionColumna = 'c';
-                case 3:
-                    posicionColumna = 'e';
-                case 4:
-                    posicionColumna = 'g';
-            }
-        }
-        return posicionColumna;
-    }
-
-    private Posicion crearPosicionInicial(){
-
-        int posicionFila = 0;
-        char posicionColumnas = ' ';
-
-        if(color.toString().equals("Blanco")){
+        if (color.toString().equalsIgnoreCase("Blanco")) {
             posicionFila = (int) Math.floor((Math.random() * 3) + 1); // Genera un número aleatorio entre 1 y 3
-        }
-        else if (color.toString().equals("Negro")){
+        } else if (color.toString().equalsIgnoreCase("Negro")) {
             posicionFila = (int) Math.floor((Math.random() * 3) + 6); // Genera un número aleatorio entre 6 y 8
         }
 
-        posicionColumnas = columnaAleatoria((posicionFila));
+        if (posicionFila % 2 == 0) {
+            switch (columnaAleatoria) {
+                case 1:
+                    posicionColumna = 'b';
+                    break;
+                case 2:
+                    posicionColumna = 'd';
+                    break;
+                case 3:
+                    posicionColumna = 'f';
+                    break;
+                case 4:
+                    posicionColumna = 'h';
+                    break;
+            }
+        } else {
+            switch (columnaAleatoria) {
+                case 1:
+                    posicionColumna = 'a';
+                    break;
+                case 2:
+                    posicionColumna = 'c';
+                    break;
+                case 3:
+                    posicionColumna = 'e';
+                    break;
+                case 4:
+                    posicionColumna = 'g';
+                    break;
+            }
+        }
 
-        Posicion posicionAleatoria = new Posicion(posicionFila, posicionColumnas);
+        Posicion posicionAleatoria = new Posicion(posicionFila, posicionColumna);
 
         return posicionAleatoria;
     }
 
-    public void mover(Direccion direccion, int pasos){
+    public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
 
-        if (direccion == null) { // Apartado 5.1
-            throw new NullPointerException("ERROR: La dirección no puede ser nula.");
-        }
-        // Apartado 5.1.1 (FALTA)
+        int nuevaFila = posicion.getFila();
+        char nuevaLetra = posicion.getColumna();
+        //this.esDamaEspecial = true;
 
+        int ejeX = 0; // 0 -> Derecha | 1 -> Izquierda || Variable auxiliar para ver la dirreción de la ficha
+
+        //Validaciones
+        Objects.requireNonNull(direccion, "ERROR: La dirección no puede ser nula.");
         if (pasos <= 0) { // Apartado 5.2
             throw new IllegalArgumentException("El número de pasos debe ser mayor que cero.");
         }
-        if (!this.esDamaEspecial && pasos > 1){ // Apartado 5.2.1.
-            throw new IllegalArgumentException("Movimiento no valido: No es una dama especial");
+        if (!this.esDamaEspecial && pasos != 1) { // Apartado 5.2.1.
+            throw new OperationNotSupportedException("ERROR: Las damas normales solo se pueden mover 1 casilla.");
         }
-        if ((color.toString().equals("Blanco") && posicion.getFila() == 8) || // Apartado 5.3
-                (color.toString().equals("Negro") && posicion.getFila() == 1)){
+        if ((color.toString().equalsIgnoreCase("Blanco") && posicion.getFila() == 8) || // Apartado 5.3
+                (color.toString().equalsIgnoreCase("Negro") && posicion.getFila() == 1)) {
             this.esDamaEspecial = true;
         }
+        if (!this.esDamaEspecial) { // Apartado 5.4
+            if (color.toString().equalsIgnoreCase("Blanco") && (direccion.toString().equalsIgnoreCase("SURESTE") || direccion.toString().equalsIgnoreCase("SUROESTE"))) {
+                throw new OperationNotSupportedException("ERROR: Movimiento no permitido.");
+            }
+            if (color.toString().equalsIgnoreCase("Negro") && (direccion.toString().equalsIgnoreCase("NORESTE") || direccion.toString().equalsIgnoreCase("NOROESTE"))) {
+                throw new OperationNotSupportedException("ERROR: Movimiento no permitido.");
+            }
+        }
 
+        // Método Mover
+        if (this.esDamaEspecial) {
+            if ((direccion.toString().equalsIgnoreCase("NOROESTE") || (direccion.toString().equalsIgnoreCase("NORESTE")))) {
+                nuevaFila += pasos;
+            } else {
+                nuevaFila -= pasos;
+            }
+        } else if (color.toString().equalsIgnoreCase("Blanco")) {
+            nuevaFila++;
+        } else if (color.toString().equalsIgnoreCase("Negro")) {
+            nuevaFila--;
+        }
+
+        if(direccion.toString().equalsIgnoreCase("NOROESTE") || direccion.toString().equalsIgnoreCase("SUROESTE")){
+            ejeX = 1;
+        }
+        for (int i = pasos; i != 0; i--) {
+            switch (ejeX) {
+                case 0:
+                    switch (nuevaLetra) {
+                        case 'a':
+                            nuevaLetra = 'b';
+                            break;
+                        case 'b':
+                            nuevaLetra = 'c';
+                            break;
+                        case 'c':
+                            nuevaLetra = 'd';
+                            break;
+                        case 'd':
+                            nuevaLetra = 'e';
+                            break;
+                        case 'e':
+                            nuevaLetra = 'f';
+                            break;
+                        case 'f':
+                            nuevaLetra = 'g';
+                            break;
+                        case 'g':
+                            nuevaLetra = 'h';
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (nuevaLetra) {
+                        case 'b':
+                            nuevaLetra = 'a';
+                            break;
+                        case 'c':
+                            nuevaLetra = 'b';
+                            break;
+                        case 'd':
+                            nuevaLetra = 'c';
+                            break;
+                        case 'e':
+                            nuevaLetra = 'd';
+                            break;
+                        case 'f':
+                            nuevaLetra = 'e';
+                            break;
+                        case 'g':
+                            nuevaLetra = 'f';
+                            break;
+                        case 'h':
+                            nuevaLetra = 'g';
+                            break;
+                    }
+                    break;
+            }
+        }
+        try {
+            posicion = new Posicion(nuevaFila, nuevaLetra);
+        } catch (IllegalArgumentException e) {
+            throw new OperationNotSupportedException("ERROR: Movimiento no permitido.");
+        }
     }
 
     public String toString() { // Apartado 6
--- src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -27,7 +27,7 @@ public class Posicion {
         return fila;
     }
 
-    public int getColumna(){
+    public char getColumna(){
         return columna;
     }
 
