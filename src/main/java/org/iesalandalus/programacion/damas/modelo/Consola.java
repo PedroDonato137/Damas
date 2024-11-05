17087669983a0420c26b9e995c679f7e5047f378
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-05 17:23:54 +0100

    Apartado 5.2: Creación del menú del juego

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Consola.java
@@ -6,4 +6,16 @@ public class Consola {
     private Consola(){ // Apartado 5.1
 
     }
+
+    public void mostrarMenu(){
+        System.out.println("------------------------------------------");
+        System.out.println("--              MENÚ DEL JUEGO          --");
+        System.out.println("------------------------------------------");
+        System.out.println("--                                      --");
+        System.out.println("-- 1. Crear dama por defecto            --");
+        System.out.println("-- 2. Crear dama eligiendo color        --");
+        System.out.println("-- 3. Mover                             --");
+        System.out.println("-- 4. Salir                             --");
+        System.out.println("------------------------------------------");
+    }
 }
