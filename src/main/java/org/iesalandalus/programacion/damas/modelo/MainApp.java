43ac4ca7a693e2c9a8d52dbf4e146814ab9d225d
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-06 18:31:35 +0100

    Apartado 6.2: Creación del método ejecutarOpciones, aun no he creado los demas métodos, por lo que esta incompleto

--- src/main/java/org/iesalandalus/programacion/damas/modelo/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/MainApp.java
@@ -4,4 +4,14 @@ public class MainApp {
 
     private Dama ficha;  // Apartado 6.1
 
+    private void ejecutarOpcion(int opcion){ //Apartado 6.2
+
+        switch (opcion){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+        }
+    }
+
 }
