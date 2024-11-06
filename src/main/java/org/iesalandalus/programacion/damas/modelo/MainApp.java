b4405d8b4bd7330002eda3c07b519259ed700de4
Pedro Donato <pedrodonatogarcia@gmail.com>
2024-11-06 18:35:32 +0100

    Apartado 6.3: Creación del metodo crear dama por defecto

--- src/main/java/org/iesalandalus/programacion/damas/modelo/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/MainApp.java
@@ -7,11 +7,19 @@ public class MainApp {
     private void ejecutarOpcion(int opcion){ //Apartado 6.2
 
         switch (opcion){
-            case 1:
+            case 1: // Apartado 6.3
+                // Crear dama por defecto
+                crearDamaDefecto();
             case 2:
+                // Crear dama eligiendo el color
             case 3:
+                // Mover
             case 4:
+                // Salir
         }
+    }
+    private void crearDamaDefecto(){ // Apartado 6.3
+        ficha = new Dama();
     }
 
 }
